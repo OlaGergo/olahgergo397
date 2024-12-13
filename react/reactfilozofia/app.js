abef60161aa21eb0830776ce6a7f9c46fad6beee
OlaGergo <olager397@hengersor.hu>
2024-12-13 11:16:16 +0100

    Update app.js
    react és natív különbségei

--- react/reactfilozofia/app.js
+++ react/reactfilozofia/app.js
@@ -1,10 +1,11 @@
+//natív->hogyan?
 const btn =document.createElement('button')
 btn.onclick=function(){
     alert("ez egy natív kód");
 }
 btn.innerHTML="Nativ"
 document.getElementById("nativ-button-container").appendChild(btn);
-
+// react-> mit akarok látni?
 const gomb = React.createElement("button",
 {
     onclick:function()
@@ -14,4 +15,6 @@ const gomb = React.createElement("button",
 },
 "React gomb"
 )
+//első paramáter az elemet amit használni szeretnénk,
+//második paraméter egy referencia egy natív DOM elemre, hivtkozásipont megadása
 ReactDOM.render(gomb,document.getElementById("react-button-container"));
