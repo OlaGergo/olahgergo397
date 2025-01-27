774bfcffbb6f30296809eee119b941d4bbbca8cc
OlaGergo <olager397@hengersor.hu>
2025-01-27 12:11:42 +0100

    mysqlServer

--- oraimunka/kozut/backend/index.js
+++ oraimunka/kozut/backend/index.js
@@ -3,5 +3,6 @@ const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-app.use (bodyParser.json());
+app.use(bodyParser.json());
+
 app.use(cors());
