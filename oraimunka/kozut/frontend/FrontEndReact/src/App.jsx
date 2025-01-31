098be8566c452bd9c6039d1210e0c2b977812d68
OlaGergo <olager397@hengersor.hu>
2025-01-31 14:19:08 +0100

    react

---
+++ oraimunka/kozut/frontend/FrontEndReact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Közutak</h1>
+      
+    </>
+  )
+}
+
+export default App
